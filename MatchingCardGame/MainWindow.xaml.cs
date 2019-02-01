87bc16efa14692992247f7d66f06d26482a9e9d6
Andrew McKernan <andrewmck96@gmail.com>
2019-02-01 00:34:42 -0700

    Initial commit, added a few base parts, not compiling at the moment

--- MatchingCardGame/MainWindow.xaml.cs
+++ MatchingCardGame/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,37 @@ namespace MatchingCardGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Regex isNumberRegex = new Regex("^[0-9]*");
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isTextAllowed(numberOfPairsBox.Text.ToString()))
+            {
+                startGame();
+            }
+        }
+
+        // check if the number of pairs entered is valid
+        private bool isTextAllowed(string text)
+        {
+            return isNumberRegex.IsMatch(text);
+        }
+
+        private void startGame()
+        {
+
+        }
+
+        private void numberOfPairsBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            //if (!isTextAllowed(e.Text.ToString()))
+            //{
+            //    e.Text = "";
+            //}
         }
     }
 }
